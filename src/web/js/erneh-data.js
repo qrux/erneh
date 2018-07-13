d978ad93b2c74d78512be2458b946f1fb8c7ec63
Qrux <qrux@xlapp.org>
2018-07-13 16:29:57 +0100

    Broke test-data into separate file.

---
+++ src/web/js/erneh-data.js
@@ -0,0 +1,122 @@
+var meridian = {};
+
+meridian['00'] = {
+    name: "Sale Mara",
+    number: "00",
+    gender: 'm',
+    symbol: 'Q'
+};
+meridian['13'] = {
+    name: "Ana Vacas",
+    number: "13",
+    gender: 'f',
+    symbol: 'Av'
+};
+meridian['21'] = {
+    name: "Natalie Yeoh",
+    number: "21",
+    gender: 'f',
+    symbol: 'Ny'
+};
+meridian['6'] = {
+    name: "Pip Proctor",
+    number: "6",
+    gender: 'f',
+    symbol: 'Pp'
+};
+meridian['25'] = {
+    name: "Polly Aleksandrova",
+    number: "25",
+    gender: 'f',
+    symbol: 'Pa'
+};
+meridian['7'] = {
+    name: "Sara Liz Marty",
+    number: "7",
+    gender: 'f',
+    symbol: 'SL'
+};
+meridian['89'] = {
+    name: "Sian Walker",
+    number: "89",
+    gender: 'f',
+    symbol: 'Sw'
+};
+meridian['4'] = {
+    name: "Andrew Potts",
+    number: "4",
+    gender: 'm',
+    symbol: 'Ps'
+};
+meridian['17'] = {
+    name: "Matt McFeeley",
+    number: "17",
+    gender: 'm',
+    symbol: 'Mm'
+};
+meridian['8'] = {
+    name: "Paddy Bunker",
+    number: "8",
+    gender: 'm',
+    symbol: 'Pb'
+};
+meridian['2'] = {
+    name: "Sam Corbett",
+    number: "2",
+    gender: 'm',
+    symbol: 'Pi'
+};
+meridian['10'] = {
+    name: "Samuel Cryan",
+    number: "10",
+    gender: 'm',
+    symbol: 'Pt'
+};
+meridian['11'] = {
+    name: "Will Mabey",
+    number: "11",
+    gender: 'm',
+    symbol: 'Wm'
+};
+meridian['30'] = {
+    name: "Toks Adesanya",
+    number: "30",
+    gender: 'm',
+    symbol: 'Tk'
+};
+meridian['5'] = {
+    name: "Russell Eassom",
+    number: "5",
+    gender: 'm',
+    symbol: 'Re'
+};
+meridian['24'] = {
+    name: "Yohann Sudell",
+    number: "24",
+    gender: 'm',
+    symbol: 'Yo'
+};
+meridian['14'] = {
+    name: "Max Inchcoombe",
+    number: "14",
+    gender: 'm',
+    symbol: 'Mx'
+};
+meridian['69'] = {
+    name: "Will Withall",
+    number: "69",
+    gender: 'm',
+    symbol: 'Jr'
+};
+meridian['102'] = {
+    name: "Benjamin Rees",
+    number: "102",
+    gender: 'm',
+    symbol: 'Bj'
+};
+meridian['101'] = {
+    name: "Zara Williams",
+    number: "101",
+    gender: 'f',
+    symbol: 'Zw'
+};
